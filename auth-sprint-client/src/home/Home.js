e736f35d56070080506b663cdd1a0e45e0774d92
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-05 13:52:49 -0400

    Modifies the <h1> on the home page

--- auth-sprint-client/src/home/Home.js
+++ auth-sprint-client/src/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return(
         <div>
-            <h1>Welcome to the Dad Jokes Home Page!</h1>
+            <h1>Welcome to the Home Page of Tico's Dad Jokes!</h1>
             <h2>This is the client-side of my Authentication Sprint Challenge!</h2>
             <h3>Sign-in to see some jokes!</h3>
         </div>
