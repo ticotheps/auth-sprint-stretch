8e8369534b09f0c0849dcd444cdd69cca78e919f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-05 13:32:09 -0400

    Adds a new <Home /> component and creates a <NavLink> and a <Route> (with render props) for each component inside of App.js

--- auth-sprint-client/src/App.js
+++ auth-sprint-client/src/App.js
@@ -11,6 +11,48 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>
+          <nav>
+            <NavLink exact to="/">Home</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/login">Sign-In</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/register">Sign-Up</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/jokes">Jokes</NavLink>
+          </nav>
+        </header>
+        <main>
+        <Route
+            exact
+            path="/"
+            render={props => {
+              console.log(props);
+              return <Home {...props} />;
+            }}
+          />
+          <Route
+            path="/login"
+            render={props => {
+              console.log(props);
+              return <Login {...props} />;
+            }}
+          />
+          <Route
+            path="/register"
+            render={props => {
+              console.log(props);
+              return <Signup {...props} />;
+            }}
+          /> 
+          <Route
+            path="/jokes"
+            render={props => {
+              console.log(props);
+              return <Jokes {...props} />;
+            }}
+          />
+        </main>
       </div>
     );
   }
